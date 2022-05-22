100a2b774cd60ecf67a52bc70734b1866bd0995a
F1r0x <103068924+F1r0x@users.noreply.github.com>
2022-05-23 00:21:29 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// sidebar toggle
-const btnToggle = document.querySelector('.toggle-btn');
-
-btnToggle.addEventListener('click', function () {
-  console.log('clik')
-  document.getElementById('sidebar').classList.toggle('active');
-  console.log(document.getElementById('sidebar'))
-});
