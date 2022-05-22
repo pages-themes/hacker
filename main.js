da765b4d61d47cf1d8f0c39917cfddd1a75f1b30
F1r0x <103068924+F1r0x@users.noreply.github.com>
2022-05-22 03:36:16 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// sidebar toggle
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
